c34d65f978cb1d704e115e500b01911a215a1b40
Unkus <unkuses@gmail.com>
2025-07-27 09:26:55 -0500

    seporate statistic message into small messages

--- BossBot/Commands/ActivityLogger/GetUserStatistics.cs
+++ BossBot/Commands/ActivityLogger/GetUserStatistics.cs
@@ -10,9 +10,22 @@ public class GetUserStatistics(UserStatisticData userStatisticData) : ICommand
     public async Task<List<string>> ExecuteAsync(ulong chatId, ulong userId, string[] commands)
     {
         var usersInfo = userStatisticData.GetUserStatistics(chatId);
+        List<StringBuilder> resultList = [];
         var result = new StringBuilder();
+        resultList.Add(result);
         var maxUserNameLength = usersInfo.Max(u => u.UserName.Length) - 3;
-        usersInfo.ForEach(u => result.AppendLine($"{StringHelper.PopulateWithWhiteSpaces(u.UserId.ToString(), 2)} | {StringHelper.PopulateWithWhiteSpaces(u.UserName, maxUserNameLength)} | {u.Count}"));
-        return [result.ToString()];
+        
+        usersInfo.ForEach(u =>
+        {
+            
+            var str = $"{StringHelper.PopulateWithWhiteSpaces(u.UserId.ToString(), 2)} | {StringHelper.PopulateWithWhiteSpaces(u.UserName, maxUserNameLength)} | {u.Count}";
+            if ((result.Length + str.Length) > 2000)
+            {
+                result = new StringBuilder();
+                resultList.Add(result);
+            }
+            result.AppendLine(str);
+        });
+        return resultList.Select(r => r.ToString()).ToList();
     }
 }
